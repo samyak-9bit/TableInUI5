ffd3a92249da5a2eda6373d2e1e69219ac4d3ebc
Samyak Patni <samyak.patni@ninebit.in>
2024-03-15 18:11:12 +0530

    Data Fetched

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,9 +1,28 @@
-sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController, MessageBox) {
-	"use strict";
+sap.ui.define(["./BaseController", "sap/ui/model/json/JSONModel"], function (BaseController, JSONModel) {
+    "use strict";
 
-	return BaseController.extend("com.myorg.myapp.controller.Main", {
-		sayHello: function () {
-			MessageBox.show("Hello World!");
-		}
-	});
-});
+    return BaseController.extend("com.myorg.myapp.controller.Main", {
+        onInit: function () {
+            var oModel = new JSONModel();
+            this.getView().setModel(oModel, "people");
+            this.fetchData();
+         },
+         fetchData: function(){
+            var sApiUrl = "https://services.odata.org/TripPinRESTierService/(S(id))/People";
+            jQuery.ajax({
+               url: sApiUrl,
+               method: "GET",
+               dataType: "json",
+               success: function (oData) {
+                  console.log(oData)
+                  this.getView().getModel("people").setData(oData);
+               }.bind(this),
+               error: function (error) {
+                  console.error("Error fetching API data:", error);
+               }
+            });
+         }
+        });
+    })
+
+
