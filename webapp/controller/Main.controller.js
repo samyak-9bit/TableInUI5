4b77d96548fff6fd3f0ae17de87afa54ddbde0b3
Samyak Patni <samyak.patni@ninebit.in>
2024-03-19 18:00:57 +0530

    Table Created

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -4,18 +4,18 @@ sap.ui.define(["./BaseController", "sap/ui/model/json/JSONModel"], function (Bas
     return BaseController.extend("com.myorg.myapp.controller.Main", {
         onInit: function () {
             var oModel = new JSONModel();
-            this.getView().setModel(oModel, "people");
+            this.getView().setModel(oModel, "product");
             this.fetchData();
          },
          fetchData: function(){
-            var sApiUrl = "https://services.odata.org/TripPinRESTierService/(S(id))/People";
+            var sApiUrl = "https://fakestoreapi.com/products";
             jQuery.ajax({
                url: sApiUrl,
                method: "GET",
                dataType: "json",
                success: function (oData) {
                   console.log(oData)
-                  this.getView().getModel("people").setData(oData);
+                  this.getView().getModel("product").setData(oData);
                }.bind(this),
                error: function (error) {
                   console.error("Error fetching API data:", error);
